4422b6b1ea1db5a39ac7c35d0191a3228eb337d4
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-06-28 18:09:08 -0400

    commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Show the pop-up window
+    var popup = document.getElementById("language-popup");
+    popup.style.display = "flex"; // Use flex to center the content
+});
